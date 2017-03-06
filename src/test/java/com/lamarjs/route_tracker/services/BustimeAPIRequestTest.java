de5f01f7f5c8b78b74ee4e96e1634752f6433069
Lamar J. Smith <smith.lamarj@gmail.com>
2017-03-06 15:43:30 -0600

    Started testing prep.

---
+++ src/test/java/com/lamarjs/route_tracker/services/BustimeAPIRequestTest.java
@@ -0,0 +1,21 @@
+package com.lamarjs.route_tracker.services;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class BustimeAPIRequestTest extends junit.framework.TestSuite{
+
+	BustimeAPIRequest request;
+	@Before
+	public void setUp() throws Exception {
+		request = new BustimeAPIRequest();
+	}
+
+	@Test
+	public void test() {
+		// TODO implement tests
+	}
+}
